6d415b10f9c2a7d89d29b428ed914b1ae679d682
PetitCakeAuxOlives <morgan.rdr@gmail.com>
2022-04-21 10:06:20 +0200

    ajout du carousel au header component

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,25 +10,26 @@ export class ApiService {
   constructor(public http:HttpClient) { }
 
   chuckApi = 'https://api.chucknorris.io/jokes/random';
+  urlPicsum = "https://picsum.photos";
 
   getUrl(url:string){
     return this.http.get(url);
   }
 
   listPicsum(num:number){
-    return 'https://picsum.photos/v2/list?page='+num+'&limit=9';
+    return this.urlPicsum+'/v2/list?page='+num+'&limit=9';
   }
 
   imgPicsum(id:number){
-    return 'https://picsum.photos/id/' +id+ '/640/480';
+    return this.urlPicsum+'/id/' +id+ '/640/480';
   }
 
   bigImgPicsum(id:number){
-    return 'https://picsum.photos/id/' +id+ '/1280/960';
+    return this.urlPicsum+ '/id+' + id +'/1280/960';
   }
 
   imgInfo(id:number){
-    return 'https://picsum.photos/id/'+ id +'/info';
+    return this.urlPicsum+ '/id/'+ id +'/info';
   }
 
 

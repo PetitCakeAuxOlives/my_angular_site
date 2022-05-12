53ddd433bb64223b8ff7ba63851becd07e153b9f
PetitCakeAuxOlives <morgan.rdr@gmail.com>
2022-05-12 09:40:46 +0200

    Import Laravel api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   constructor(public http:HttpClient) { }
 
   chuckApi = 'https://api.chucknorris.io/jokes/random';
-  urlPicsum = "https://picsum.photos";
+  urlPicsum = "127.0.0.1:8000/api/picture";
   urlPodek = 'http://dekpo.com/api/public/pictures/list?page=19&limit=3'
 
   getUrl(url:string){
@@ -18,7 +18,7 @@ export class ApiService {
   }
 
   listPicsum(num:number){
-    return this.urlPicsum+'/v2/list?page='+num+'&limit=9';
+    return this.urlPicsum+'?page='+num+'&limit=9';
   }
 
   imgPicsum(id:number){
@@ -32,6 +32,5 @@ export class ApiService {
   imgInfo(id:number){
     return this.urlPicsum+ '/id/'+ id +'/info';
   }
-
 
 }
